471fe925eaa1d025bdfc76aa1539b1a12dbfbc33
myarth <fly8859@126.com>
2015-11-05 17:12:07 +0800

    @

--- app/src/main/groovy/com/myth/myarth/gradleapplication/utils/SharedPreferencesUtil.groovy
+++ app/src/main/groovy/com/myth/myarth/gradleapplication/utils/SharedPreferencesUtil.groovy
@@ -9,26 +9,38 @@ import groovy.transform.CompileStatic
 @CompileStatic
 class SharedPreferencesUtil {
 
-    private static final String SP_NAME = 'gradleapplication'
-
     static final String FIRST_TIME_USE = 'first-time-use'
     static final String TOKEN = 'token'
 
-    static SharedPreferences getSharedPreferences() {
-        AppContext.instance.getSharedPreferences(SP_NAME, Context.MODE_PRIVATE)
+    private static SharedPreferences sp
+    private static final String SP_NAME = 'gradleapplication'
+
+    static synchronized SharedPreferences getSharedPreferences(Context context) {
+        if (sp == null) {
+            synchronized (SharedPreferences) {
+                if (sp == null) {
+                    sp = context.getSharedPreferences(SP_NAME, Context.MODE_PRIVATE)
+                }
+            }
+        }
+        sp
     }
 
-    static boolean getBoolean(String key, boolean defValue) {
-        def value = sharedPreferences.getBoolean(key, defValue)
+    static boolean getBoolean(String key, boolean defValue, Context context = AppContext.instance) {
+        def value = getSharedPreferences(context).getBoolean(key, defValue)
         value != null ? value : defValue
     }
 
-    static void putBoolean(String key, boolean value) {
-        sharedPreferences.edit()?.putBoolean(key, value)?.apply()
+    static void putBoolean(String key, boolean value, Context context = AppContext.instance) {
+        getSharedPreferences(context).edit().putBoolean(key, value).apply()
     }
 
-    static String getString(String key, String defValue) {
-        sharedPreferences.getString(key, defValue)
+    static String getString(String key, String defValue, Context context = AppContext.instance) {
+        getSharedPreferences(context).getString(key, defValue)
+    }
+
+    static void putString(String key, String value, Context context = AppContext.instance) {
+        getSharedPreferences(context).edit().putString(key, value).apply()
     }
 
 }
